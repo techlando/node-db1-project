0d5c6604b830d3a64671b37c87b30b3bef714550
Landon Phillips <landonphillips@Landons-MacBook-Pro.local>
2022-05-16 23:18:24 -0500

    first

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,29 @@
+const Account = require('./accounts-model');
+
 exports.checkAccountPayload = (req, res, next) => {
+  console.log('here we are 1')
+  next()
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
+  console.log('here we are 2')
+  next()
   // DO YOUR MAGIC
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+ try {
+  const account = await Account.getById(req.params.id)
+  if(!account) {
+    next({status: 404, message: 'not found'})
+  } else {
+    req.account = account
+    next()
+  }
+ } catch (err) {
+  next(err)
+ }
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,52 @@
-const router = require('express').Router()
-
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+const router = require('express').Router();
+const md = require('./accounts-middleware');
+const Account = require('./accounts-model');
+router.get('/', async (req, res, next) => {
+  // DO YOUR MAGIC 
+  try {
+    const accounts = await Account.getAll()
+    res.json(accounts)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id',  md.checkAccountId, async (req, res, next) => {
+res.json(req.account)
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', md.checkAccountPayload, md.checkAccountNameUnique, (req, res, next) => {
+  // DO YOUR MAGIC 
+  try {
+
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', md.checkAccountId, md.checkAccountPayload, md.checkAccountNameUnique, (req, res, next) => {
+  // DO YOUR MAGIC 
+  try {
+
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', md.checkAccountId, (req, res, next) => {
+  // DO YOUR MAGIC 
+  try {
+
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  // DO YOUR MAGIC 
+  res.status(err.status || 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
