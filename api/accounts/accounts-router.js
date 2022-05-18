e06876f424a648df80e348b6a99c9fda85feda25
Landon Phillips <landonphillips@Landons-MBP.lan1>
2022-05-18 18:35:02 -0500

    finished mvp

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -15,35 +15,39 @@ router.get('/:id',  md.checkAccountId, async (req, res, next) => {
 res.json(req.account)
 })
 
-router.post('/', md.checkAccountPayload, md.checkAccountNameUnique, (req, res, next) => {
-  // DO YOUR MAGIC 
+router.post('/', md.checkAccountPayload, md.checkAccountNameUnique, async (req, res, next) => {
+  
   try {
-
+    const newAccount = await Account.create({ 
+      name: req.body.name.trim(), budget: req.body.budget})
+    res.status(201).json(newAccount)
   } catch (err) {
     next(err)
   }
 })
 
-router.put('/:id', md.checkAccountId, md.checkAccountPayload, md.checkAccountNameUnique, (req, res, next) => {
-  // DO YOUR MAGIC 
+router.put('/:id', md.checkAccountId, md.checkAccountPayload, async  (req, res, next) => {
+  
   try {
-
+    const updated = await Account.updateById(req.params.id, req.body)
+  res.json(updated)
   } catch (err) {
     next(err)
   }
 });
 
-router.delete('/:id', md.checkAccountId, (req, res, next) => {
-  // DO YOUR MAGIC 
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
+ 
   try {
-
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
   } catch (err) {
     next(err)
   }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC 
+  
   res.status(err.status || 500).json({
     message: err.message
   })
